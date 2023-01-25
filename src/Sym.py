c454211e90402a597a514b94292ef0b76d00b60a
Vasu Agrawal <vasuagwl@gmail.com>
2023-01-25 04:59:37 -0500

    Converted the class to Python

--- src/Sym.py
+++ src/Sym.py
@@ -1,23 +1,32 @@
-# -- ### SYM
-# -- Summarize a stream of symbols.
-# function SYM.new(i,at,txt) --> SYM; constructor
-#   i.at, i.txt = at or 0, txt or "" -- col position and name
-#   i.n   = 0
-#   i.has = {}
-#   i.most, i.mode = 0,nil end
+import math
 
-# function SYM.add(i,x) --> nil;  update counts of things seen so far
-#   if x ~= "?" then 
-#    i.n = i.n + 1 
-#    i.has[x] = 1 + (i.has[x] or 0)
-#    if i.has[x] > i.most then
-#      i.most,i.mode = i.has[x], x end end end 
+class sym:
 
-# function SYM.mid(i,x) return i.mode end --> n; return the mode
+    def __init__(self, at=0, txt= ''):
+        self.at = at
+        self.txt = txt
+        self.n = 0
+        self.has = {}
+        self.most = 0
+        self.mode = None
 
-# function SYM.div(i,x,    fun,e) --> n; return the entropy
-#   function fun(p) return p*math.log(p,2) end
-#   e=0; for _,n in pairs(i.has) do e = e + fun(n/i.n) end 
-#   return -e end
+    def add(self,x):
+        if x != '?':
+            self.n+=1
+            self.has = 1 + (self.has[0] if self.has else 0)
+            if self.has[x] > self.most:
+                self.most,self.mode = self.has[x],x
 
-# function SYM.rnd(i,x,n) return x end --> s; return `n` unchanged (SYMs do not get rounded)
+    def mid(self,x):
+        return self.mode
+
+    def div(self,x,e):
+        def fun(p):
+            return p*math.log(p,2)
+        e = 0
+        for _,n in self.has:
+            e += fun(n/self.n)
+        return -e
+
+    def rnd(self,i,x,n):
+        return x
