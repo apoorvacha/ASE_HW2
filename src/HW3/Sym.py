d7688540c40db7aa9512c225a8719c6440ec85cc
Hrushabhsingh Chouhan <chouhan.hrushabh@gmail.com>
2023-02-02 11:19:30 -0500

    updated Sym.py

--- src/HW3/Sym.py
+++ src/HW3/Sym.py
@@ -1,30 +1,3 @@
-
-# function SYM.new(i,at,txt) --> SYM; constructor
-#   i.at, i.txt = at or 0, txt or "" -- col position and name
-#   i.n   = 0
-#   i.has = {}
-#   i.most, i.mode = 0,nil end
-
-# function SYM.add(i,x) --> nil;  update counts of things seen so far
-#   if x ~= "?" then 
-#    i.n = i.n + 1 
-#    i.has[x] = 1 + (i.has[x] or 0)
-#    if i.has[x] > i.most then
-#      i.most,i.mode = i.has[x], x end end end 
-
-# function SYM.mid(i,x) return i.mode end --> n; return the mode
-
-# function SYM.div(i,x,    fun,e) --> n; return the entropy
-#   function fun(p) return p*math.log(p,2) end
-#   e=0; for _,n in pairs(i.has) do e = e + fun(n/i.n) end 
-#   return -e end
-
-# function SYM.rnd(i,x,n) return x end --> s; return `n` unchanged (SYMs do not get rounded)
-
-# function SYM.dist(i,s1,s2)
-#   return s1=="?" and s2=="?" and 1 or (s1==s2) and 0 or 1 end
-
-
 import math
 
 class Sym:
@@ -57,11 +30,9 @@ class Sym:
         def fun(p):
             return p*math.log(p,2)
         e = 0
-        for n in self.has:
-            p = self.has[n] / self.n
-            e += fun(p)
+        for k, v in self.has.items():
+            e = e + fun(v/self.n)
         return -e
-
 
     def rnd(self,i,x,n):
         return x
