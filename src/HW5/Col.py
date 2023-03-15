b0402710bf5b9e4d80df64157794b29765996b89
Vasu Agrawal <vasuagwl@gmail.com>
2023-03-15 14:59:26 -0400

    added col and cols

--- src/HW5/Col.py
+++ src/HW5/Col.py
@@ -4,3 +4,14 @@
 #    col.isKlass    = col.txt:find"!$"
 #    col.isGoal     = col.txt:find"[!+-]$"
 #    return col end
+from Num import *
+from Sym import *
+import re
+
+class Col:
+    def __init__(self, n, s):
+
+        self.col = Num(n, s) if re.match("^[A-Z]",s) else Sym(n, s)
+        self.isIgnored = self.col.txt.endswith("X")
+        self.isKlass = self.col.txt.endswith("!")
+        self.isGoal = self.col.txt[-1] in ["!", "+", "-"]
--- src/HW5/Cols.py
+++ src/HW5/Cols.py
@@ -1,39 +1,30 @@
-import re
-from Num import *
-from Sym import *
+'''
+function COLS(ss,     col,cols)
+  cols={names=ss, all={},x={},y={}}
+  for n,s in pairs(ss) do  
+    col = push(cols.all, COL(n,s))
+    if not col.isIgnored then
+      if col.isKlass then cols.klass = col end
+      push(col.isGoal and cols.y or cols.x, col) end end 
+  return cols ends
+'''
+from src.col import Col  # type: ignore
+
 
 class Cols:
-     def __init__(self, t):
-        self.names = t
+    def __init__(self, ss):
+        
+        self.names = ss
         self.all = []
         self.x = []
         self.y = []
-        self.klass = None 
-
-        for n, s in enumerate(t):
-            print("here",s)
-            col = Num(n,s) if re.match("^[A-Z]", s) else Sym(n,s)
+        for i, val in enumerate(ss):
+            col = Col(i, val)
             self.all.append(col)
-            if not re.match(".*X$",s):
-                if re.match("!$",s):
-                    self.klass = col
-                if re.match('.*\+$',s) or re.match('.*\-$',s) or re.match('.*\!$',s):
+            if not col.isIgnored:
+                if col.isKlass:
+                    col.isKlass = col
+                if col.isGoal:
                     self.y.append(col)
                 else:
                     self.x.append(col)
-    
-     def add(self, row):
-        lst = [self.x,self.y]
-        for _, t in enumerate(lst):
-            for _, col in enumerate(t):
-                col.add(row.cells[col.at])
-    
-
-# function COLS(ss,     col,cols)
-#   cols={names=ss, all={},x={},y={}}
-#   for n,s in pairs(ss) do  
-#     col = push(cols.all, COL(n,s))
-#     if not col.isIgnored then
-#       if col.isKlass then cols.klass = col end
-#       push(col.isGoal and cols.y or cols.x, col) end end 
-#   return cols end
