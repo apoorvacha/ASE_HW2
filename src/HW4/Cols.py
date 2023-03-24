23229d73cf41face79a773cb29eafeb481ef50a2
Hrushabhsingh Chouhan <chouhan.hrushabh@gmail.com>
2023-03-24 19:06:39 -0400

    Updated Cols.py

--- src/HW4/Cols.py
+++ src/HW4/Cols.py
@@ -11,12 +11,12 @@ class Cols:
         self.klass = None 
 
         for n, s in enumerate(t):
-            col = Num.Num(n,s) if re.match("^[A-Z]", s) else Sym.Sym(n,s)
+            col = Num.Num(n,s) if re.match("^[A-Z]+", s) else Sym.Sym(n,s)
             self.all.append(col)
-            if not re.match(".*X$",s):
+            if not re.match("X$",s):
                 if re.match("!$",s):
                     self.klass = col
-                if re.match('.*\+$',s) or re.match('.*\-$',s) or re.match('.*\!$',s):
+                if re.match("[!+-]$",s):
                     self.y.append(col)
                 else:
                     self.x.append(col)
