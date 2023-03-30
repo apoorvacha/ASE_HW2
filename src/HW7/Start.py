b283a9d5fe00e0da6941eb0935ec8dd0913bf1ea
Apoorva Chauhan <47474447+apoorvacha@users.noreply.github.com>
2023-03-31 04:02:35 +0530

    Update Start.py

--- src/HW7/Start.py
+++ src/HW7/Start.py
@@ -1,30 +1,59 @@
+import sys, getopt
 from Examples import *
 
-def main(funs):
-    fails = 0
-    for what, _ in funs.items():
-            if funs[what]() == False:
-                fails += 1
-                print("❌ fail:",what)
-            else: print("✅ pass:",what)
-    if (fails == 0): return 0
-    else: return 1
+argumentList = sys.argv[1:]
+the = {"seed": 937162211, "dump": False, "halves":"", "reuse":True , "go": "data", "help": False, "file": "/etc/data/repgrid1.csv","min": "min", "rest":4}
+b4={}
+ENV = {}
+for k,v in ENV:
+    b4[k]=v #cache old names (so later, we can find rogues)
+# Options
+options = "hg"
+ 
+# Long options
+long_options = []
+def help():
+    a= """
+        script.lua : an example script with help text and a test suite
+        (c)2022, Tim Menzies <timm@ieee.org>, BSD-2
+        USAGE:   script.lua  [OPTIONS] [-g ACTION]
+        OPTIONS:
+        -d  --dump    on crash, dump stack   = false
+        -f  --file    name of file           = ../etc/data/repgrid1.csv
+        -g  --go      start-up action        = data
+        -h  --help    show help              = false
+        -p  --p       distance coefficient   = 2
+        -s  --seed    random number seed     = 937162211
+        ACTIONS:
+        -g  the	show settings
+        -g  rand	generate, reset, regenerate same
+        -g  sym	check syms
+        -g  num	check nums
+        """
+    print(a)
 
-egs = {}
-def eg(key, string, fun):
-     egs[key] = fun
 
-eg("ok", "seed generation", test_ok)
-eg("sample", " testing of samples", test_sample)
-eg("nums","testing nums", test_num)
-eg("gauss", "testing gaussian", test_gaussian)
-eg("boot", "testing bootstrap", test_bootstrap)
-eg("basic", "testing basic", test_basic)
-eg("pre", "testing pre", test_pre)
-eg("five", "testing five", test_five)
-eg("six", "testing six", test_six)
-eg("tiles", "testing tiles", test_tiles)
-eg("sk", "testing sk", test_sk)
+def run_tests():
+    func_pass= 0
+    test_suite = [test_ok, test_sample,test_num, test_gaussian,test_bootstrap, test_basic, test_pre, test_five, test_six, test_tiles,  test_sk]
+    # test_suite = [test_explain]
+    for i,test in enumerate(test_suite):
+        if(test()):
+            func_pass += 1
+    print("\nTotal Test Cases Passing: " + str(func_pass) + "\nTotal Test Cases Failing: " + str(len(test_suite)-func_pass))
+    
+
+def main():
+    try:
+        arguments, values = getopt.getopt(argumentList, options, long_options)
+        for currentArgument, currentValue in arguments:
+             if currentArgument in ('-h', ''):
+                help()
+             if currentArgument in ("-g", ''):
+               run_tests() 
+                
+    except getopt.error as err:
+        print (str(err))
 
 if __name__ == "__main__":
-    main(egs)
+  main()
